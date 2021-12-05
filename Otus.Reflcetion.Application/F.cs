7f210e971e66268ade3db74b528cab76c058d78b
Saiglor <jakimets.2012@gmail.com>
2021-12-05 20:48:45 +0500

    Добавил тестовый класс

---
+++ Otus.Reflcetion.Application/F.cs
@@ -0,0 +1,22 @@
+﻿namespace Otus.Reflcetion.Application
+{
+    public class F
+    {
+        private int field1, field2, field3, field4, field5;
+
+        public int Prop1 => field1;
+        public int Prop2 => field2;
+        public int Prop3 => field3;
+        public int Prop4 => field4;
+        public int Prop5 => field5;
+
+        public static F Get() => new()
+        {
+            field1 = 1,
+            field2 = 2,
+            field3 = 3,
+            field4 = 4,
+            field5 = 5
+        };
+    }
+}
