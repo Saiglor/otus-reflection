dc8f9abfed2c257cfde22644817286c0f7750992
Saiglor <jakimets.2012@gmail.com>
2021-12-05 21:40:59 +0500

    Добавил сериализацию

---
+++ Otus.Reflcetion.Application/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using Otus.Reflection.CsvSerializer;
+
+namespace Otus.Reflcetion.Application
+{
+    class Program
+    {
+        private const string Separator = ";";
+
+        static void Main()
+        {
+            var f = F.Get();
+
+            var resultSerializeMilliseconds = SerializeData(f, out var resultSerialize);
+            
+            WriteResult(resultSerialize, resultSerializeMilliseconds);
+
+            var resultToJsonMilliseconds = SerializeDataToJson(f, out var resultToJson);
+
+            WriteResult(resultToJson, resultToJsonMilliseconds);
+        }
+
+        private static long SerializeData(object data, out string result)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            result = string.Empty;
+            for (var i = 0; i < 9999; i++)
+            {
+                result = CsvSerializer.Serialize(data, Separator);
+            }
+
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        private static void WriteResult(string result, long resultMilliseconds)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine(result);
+            Console.WriteLine(resultMilliseconds);
+
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+
+        private static long SerializeDataToJson(object data, out string result)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            result = JsonSerializer.Serialize(data);
+
+            return stopwatch.ElapsedMilliseconds;
+        }
+    }
+}
---
+++ Otus.Reflection.CsvSerializer/CsvSerializer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Otus.Reflection.CsvSerializer
+{
+    public class CsvSerializer
+    {
+        /// <summary>
+        /// Сериализация объекта в csv строку
+        /// </summary>
+        /// <param name="obj">Передаваемый объект</param>
+        /// <param name="separator">Разделитель</param>
+        public static string Serialize<T>(T obj, string separator) where T : class
+        {
+            var type = obj.GetType();
+
+            var csv = new StringBuilder();
+
+            const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+            var fieldInfos = type.GetFields(bindingFlags);
+            csv.AppendLine(FieldsToCsv(fieldInfos, obj, separator));
+
+            var propertyInfos = type.GetProperties(bindingFlags);
+            csv.Append(PropertiesToCsv(propertyInfos, obj, separator));
+
+            return csv.ToString();
+        }
+        
+        private static string FieldsToCsv(FieldInfo[] fieldInfos, object obj, string separator)
+        {
+            var csv = new StringBuilder();
+
+            for (var i = 0; i < fieldInfos.Length; i++)
+            {
+                var field = fieldInfos[i];
+                csv.Append(field.Name)
+                    .Append(':')
+                    .Append(field.GetValue(obj));
+
+                if (i < fieldInfos.Length - 1)
+                    csv.Append(separator);
+            }
+
+            return csv.ToString();
+        }
+
+        private static string PropertiesToCsv(PropertyInfo[] propertyInfos, object obj, string separator)
+        {
+            var csv = new StringBuilder();
+
+            for (var i = 0; i < propertyInfos.Length; i++)
+            {
+                var property = propertyInfos[i];
+                csv.Append(property.Name)
+                    .Append(':')
+                    .Append(property.GetValue(obj));
+
+                if (i < propertyInfos.Length - 1)
+                    csv.Append(separator);
+            }
+
+            return csv.ToString();
+        }
+
+        /// <summary>
+        /// Десериализация csv строки в объект
+        /// </summary>
+        public static void Deserialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
